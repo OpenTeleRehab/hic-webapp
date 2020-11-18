c03f39c81b25554304760841c23738bc79f82c8a
Chivy Lim <chivy@web-essentials.asia>
2020-11-18 09:53:14 +0700

    [TASK] Correct table cell text wrap
    refs TRA-18

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -30,10 +30,10 @@ import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
 const CustomTable = ({ columns, rows }) => {
   const [currentPage, setCurrentPage] = useState(0);
-  const [pageSize, setPageSize] = useState(5);
+  const [pageSize, setPageSize] = useState(10);
   const [showFilter, setShowFilter] = useState(false);
 
-  const pageSizes = [5, 10, 15];
+  const pageSizes = [10, 20, 30, 40, 50];
   const rightColumns = ['action'];
   const tableColumnExtensions = [{ columnName: 'action', align: 'right', width: 120 }];
   const tableColumnVisibilityColumnExtensions = [{ columnName: 'action', togglingEnabled: false }];
