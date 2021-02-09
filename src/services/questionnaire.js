97c5016232951408e039ebc0edc292ef09f05784
Phearoth <phearoth@web-essentials.co>
2021-02-09 16:08:43 +0700

    [TASK] Implement Image Upload for Question
    refs TRA-298

--- src/services/questionnaire.js
+++ src/services/questionnaire.js
@@ -1,4 +1,5 @@
 import axios from 'utils/axios';
+import _ from 'lodash';
 
 const getQuestionnaires = payload => {
   return axios.get('/questionnaire', { params: payload })
@@ -26,7 +27,16 @@ const getQuestionnaire = (id, language) => {
 };
 
 const createQuestionnaire = (payload) => {
-  return axios.post('/questionnaire', payload)
+  const formData = new FormData();
+  formData.append('lang', payload.lang);
+  formData.append('data', JSON.stringify(payload));
+  _.map(payload.questions, (question, index) => {
+    if (question.file) {
+      formData.append(index, question.file);
+    }
+  });
+
+  return axios.post('/questionnaire', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
     .then(
       res => {
         return res.data;
@@ -38,7 +48,20 @@ const createQuestionnaire = (payload) => {
 };
 
 const updateQuestionnaire = (id, payload) => {
-  return axios.post(`/questionnaire/${id}?_method=PUT`, payload)
+  const formData = new FormData();
+  formData.append('lang', payload.lang);
+  formData.append('data', JSON.stringify(payload));
+  _.map(payload.questions, (question, index) => {
+    if (question.file) {
+      if (question.file.size) {
+        formData.append(index, question.file);
+      } else {
+        formData.append('no_changed_files[]', question.id);
+      }
+    }
+  });
+
+  return axios.post(`/questionnaire/${id}?_method=PUT`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
     .then(
       res => {
         return res.data;
