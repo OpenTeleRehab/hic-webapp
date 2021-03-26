6bc0e48b65a6d60cfc821a59bb4ecabdd18fa816
Phearoth <phearoth@web-essentials.co>
2021-03-26 16:51:52 +0700

    [IMP] Change last login datetime format
    refs TRA-160

--- src/views/Admin/TabContents/clinicAdmin.js
+++ src/views/Admin/TabContents/clinicAdmin.js
@@ -102,7 +102,7 @@ const ClinicAdmin = ({ handleEdit, handleDelete, handleSwitchStatus, type }) => 
             country: getCountryName(user.country_id, countries),
             clinic: getClinicName(user.clinic_id, clinics),
             status: <EnabledStatus enabled={!!user.enabled} />,
-            last_login: user.last_login ? moment(user.last_login).format(settings.date_format + ' hh:mm A') : '',
+            last_login: user.last_login ? moment(user.last_login).format(settings.date_format + ' HH:mm:ss') : '',
             action
           };
         })}
