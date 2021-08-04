4b5c10dd7b074d9986f2864ed0fc277af70e516e
Lina <lina@web-essentials.co>
2021-08-04 09:13:31 +0700

    [TASK] Implement exercise attachment section
    refs RRT-22

--- src/views/_public/Library/Exercise/detail.js
+++ src/views/_public/Library/Exercise/detail.js
@@ -35,13 +35,29 @@ const ExerciseDetail = () => {
     }
   }, [id, exercise]);
 
+  const handleDownload = async (event, fileName, fileUrl) => {
+    event.preventDefault();
+    const response = await fetch(fileUrl);
+    if (response.status === 200) {
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      return { success: true };
+    }
+  };
+
   return (
     <>
       <div className="mb-2 d-flex justify-content-center">
         <h3 className="text-primary font-weight-bold">{exercise.title}</h3>
       </div>
       <Row>
-        <Col sm={8} xl={8}>
+        <Col sm={7} md={8}>
           <Carousel activeIndex={index} onSelect={handleSelect} controls={mediaUploads.length > 1} indicators={mediaUploads.length > 1} className="view-exercise-carousel">
             { mediaUploads.map((mediaUpload, index) => (
               <Carousel.Item key={index}>
@@ -83,8 +99,53 @@ const ExerciseDetail = () => {
             ))}
           </div>
         </Col>
-        <Col sm={4} xl={4}>
+        <Col sm={5} md={4}>
           <h5 className="text-primary">{translate('exercise.attachments')}</h5>
+          { mediaUploads.map((mediaUpload, index) => (
+            <>
+              <Row key={index}>
+                <Col sm={6} md={6}>
+                  { mediaUpload.fileType === 'audio/mpeg' &&
+                    <div className="img-thumbnail w-100 pt-2 pl-5 pr-5 bg-light audio-wrapper">
+                      <audio controls className="w-100 mt-4">
+                        <source src={mediaUpload.url || `${process.env.REACT_APP_API_BASE_URL}/file/${mediaUpload.id}`} type="audio/ogg" />
+                      </audio>
+                    </div>
+                  }
+                  { (mediaUpload.fileType !== 'audio/mpeg' && mediaUpload.fileType !== 'video/mp4') &&
+                    <img
+                      className="d-block w-100"
+                      src={mediaUpload.url || `${process.env.REACT_APP_API_BASE_URL}/file/${mediaUpload.id}`} alt="..."
+                    />
+                  }
+                  { mediaUpload.fileType === 'video/mp4' &&
+                    <video className="w-100 img-thumbnail" controls>
+                      <source src={mediaUpload.url || `${process.env.REACT_APP_API_BASE_URL}/file/${mediaUpload.id}`} type="video/mp4" />
+                    </video>
+                  }
+                </Col>
+                <Col sm={6} md={6}>
+                  <Row>
+                    <Col sm={6} md={6} className="p-0"><strong>{translate('exercise.file_name')}:</strong></Col>
+                    <Col sm={6} md={6} className="p-0" style={{ wordWrap: 'break-word' }}>{mediaUpload.fileName.replace(/\.[^/.]+$/, '')}</Col>
+                  </Row>
+                  <Row>
+                    <Col sm={6} md={6} className="p-0"><strong>{translate('exercise.file_size')}:</strong></Col>
+                    <Col sm={6} md={6} className="p-0">{mediaUpload.fileSize || 0}</Col>
+                  </Row>
+                  <Row>
+                    <Col sm={6} md={6} className="p-0"><strong>{translate('exercise.file_type')}:</strong></Col>
+                    <Col sm={6} md={6} className="p-0">{mediaUpload.fileType}</Col>
+                  </Row>
+                </Col>
+              </Row>
+              <Row className="mb-3 mt-3">
+                <Col>
+                  <Button className="w-100" size="sm" onClick={(event) => handleDownload(event, mediaUpload.fileName, mediaUpload.url || `${process.env.REACT_APP_API_BASE_URL}/file/${mediaUpload.id}`)}>{translate('exercise.attachment.download')}</Button>
+                </Col>
+              </Row>
+            </>
+          ))}
         </Col>
       </Row>
       <Row className="mt-3 d-none">
