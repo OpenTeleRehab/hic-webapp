9f0e24d8644f081fbb8309beaeeb2e7c2737d90a
Lina <lina@web-essentials.co>
2021-08-19 16:17:00 +0700

    [IMP] Remove english language code in url
    refs RRT-14

--- src/utils/route.js
+++ src/utils/route.js
@@ -1,5 +1,10 @@
 
 export const replaceRoute = (route, lang) => {
-  const newRoute = route === '/:lang?/' ? route.replace(':lang?/', lang) : route.replace(':lang?', lang);
+  let newRoute = '';
+  if (lang === 'en') {
+    newRoute = route.replace(':lang?/', '');
+  } else {
+    newRoute = route === '/:lang?/' ? route.replace(':lang?/', lang) : route.replace(':lang?', lang);
+  }
   return newRoute;
 };
